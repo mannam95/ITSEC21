a68bb00edbddc6a226d4ee05023267e7230cc929
Meghana Rao <76180138+meghanaraobn2020@users.noreply.github.com>
2021-12-07 23:54:26 +0100

    Implemented Base Pix2Pix model
    With the help of pix2pix in tensorflow blog the entire code has been modified as per the requirement
    
    Co-Authored-By: Srinath <64306529+mannam95@users.noreply.github.com>

---
+++ model/integrate.py
@@ -0,0 +1,125 @@
+import tensorflow as tf
+
+import os
+import pathlib
+import time
+import datetime
+
+from matplotlib import pyplot as plt
+from IPython import display
+
+from pre_Process import *
+from generator import * 
+from discriminator import *
+
+dataPath = 'C:/Users/megha/Downloads/facades/facades/'
+
+# Create the training dataset
+train_dataset = tf.data.Dataset.list_files(str(dataPath + 'train/*.jpg'))
+train_dataset = train_dataset.map(load_image_train, num_parallel_calls=tf.data.AUTOTUNE)
+train_dataset = train_dataset.shuffle(BUFFER_SIZE)
+train_dataset = train_dataset.batch(BATCH_SIZE)
+
+# Create the test dataset
+test_dataset = tf.data.Dataset.list_files(str(dataPath + 'test/*.jpg'))
+test_dataset = test_dataset.map(load_image_test, num_parallel_calls=tf.data.AUTOTUNE)
+test_dataset = test_dataset.batch(BATCH_SIZE)
+
+generator = Generator()
+# tf.keras.utils.plot_model(generator, show_shapes=True, dpi=64, to_file='generator.png')
+
+discriminator = Discriminator()
+# tf.keras.utils.plot_model(discriminator, show_shapes=True, dpi=64,  to_file='discriminator.png')
+
+generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+discriminator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
+
+checkpoint_dir = './training_checkpoints'
+checkpoint_prefix = os.path.join(checkpoint_dir, "ckpt")
+checkpoint = tf.train.Checkpoint(generator_optimizer=generator_optimizer,
+                                 discriminator_optimizer=discriminator_optimizer,
+                                 generator=generator,
+                                 discriminator=discriminator)
+
+
+def generate_images(model, test_input, tar):
+  prediction = model(test_input, training=True)
+  plt.figure(figsize=(15, 15))
+
+  display_list = [test_input[0], tar[0], prediction[0]]
+  title = ['Input Image', 'Ground Truth', 'Predicted Image']
+
+  for i in range(3):
+    plt.subplot(1, 3, i+1)
+    plt.title(title[i])
+    # Getting the pixel values in the [0, 1] range to plot.
+    plt.imshow(display_list[i] * 0.5 + 0.5)
+    plt.axis('off')
+  plt.show()
+
+
+
+log_dir="logs/"
+
+summary_writer = tf.summary.create_file_writer(
+  log_dir + "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+
+@tf.function
+def train_step(input_image, target, step):
+  with tf.GradientTape() as gen_tape, tf.GradientTape() as disc_tape:
+    gen_output = generator(input_image, training=True)
+
+    disc_real_output = discriminator([input_image, target], training=True)
+    disc_generated_output = discriminator([input_image, gen_output], training=True)
+
+    gen_total_loss, gen_gan_loss, gen_l1_loss = generator_loss(disc_generated_output, gen_output, target)
+    disc_loss = discriminator_loss(disc_real_output, disc_generated_output)
+
+  generator_gradients = gen_tape.gradient(gen_total_loss,
+                                          generator.trainable_variables)
+  discriminator_gradients = disc_tape.gradient(disc_loss,
+                                               discriminator.trainable_variables)
+
+  generator_optimizer.apply_gradients(zip(generator_gradients,
+                                          generator.trainable_variables))
+  discriminator_optimizer.apply_gradients(zip(discriminator_gradients,
+                                              discriminator.trainable_variables))
+
+  with summary_writer.as_default():
+    tf.summary.scalar('gen_total_loss', gen_total_loss, step=step//1000)
+    tf.summary.scalar('gen_gan_loss', gen_gan_loss, step=step//1000)
+    tf.summary.scalar('gen_l1_loss', gen_l1_loss, step=step//1000)
+    tf.summary.scalar('disc_loss', disc_loss, step=step//1000)
+    
+
+def fit(train_ds, test_ds, steps):
+  example_input, example_target = next(iter(test_ds.take(1)))
+  start = time.time()
+
+  for step, (input_image, target) in train_ds.repeat().take(steps).enumerate():
+    if (step) % 1000 == 0:
+      display.clear_output(wait=True)
+
+      if step != 0:
+        print(f'Time taken for 1000 steps: {time.time()-start:.2f} sec\n')
+
+      start = time.time()
+
+      generate_images(generator, example_input, example_target)
+      print(f"Step: {step//1000}k")
+
+    train_step(input_image, target, step)
+
+    # Training step
+    if (step+1) % 10 == 0:
+      print('.', end='', flush=True)
+
+
+    # Save (checkpoint) the model every 5k steps
+    if (step + 1) % 5000 == 0:
+      checkpoint.save(file_prefix=checkpoint_prefix)
+      
+
+fit(train_dataset, test_dataset, steps=10000)
+
---
+++ model/lib.py
@@ -0,0 +1,9 @@
+import tensorflow as tf
+
+import os
+import pathlib
+import time
+import datetime
+
+from matplotlib import pyplot as plt
+from IPython import display
