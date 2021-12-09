0936bb5ffe74b229c4b691bd3dfd341fbf56784e
Srinath <64306529+mannam95@users.noreply.github.com>
2021-12-09 20:46:49 +0100

    Update extractMinutiae.sh

--- data/extractMinutiae.sh
+++ data/extractMinutiae.sh
@@ -1,5 +1,5 @@
 for file in /Users/ya/Documents/GitHub/ITSEC21/data/FingerPrints_1Channel/*
 do
 	echo "Processing ${file##*/}"
-	/Users/ya/Desktop/FingerPrint/bin/mindtct /Users/ya/Documents/GitHub/ITSEC21/data/FingerPrints_1Channel/${file##*/} /Users/ya/Documents/GitHub/ITSEC21/data/minutiaeExtraction/$(basename ${file##*/} | cut -d. -f1)
+	/Users/ya/Desktop/FingerPrint/bin/mindtct -b /Users/ya/Documents/GitHub/ITSEC21/data/FingerPrints_1Channel/${file##*/} /Users/ya/Documents/GitHub/ITSEC21/data/minutiaeExtraction/$(basename ${file##*/} | cut -d. -f1)
 done
