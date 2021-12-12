0811ce731f5712e6732ee09ae4ac783282267a20
Meghana Rao <76180138+meghanaraobn2020@users.noreply.github.com>
2021-12-12 15:54:35 +0100

    Downsampling images to 256

---
+++ data/Downsampling_images_to_256.py
@@ -0,0 +1,54 @@
+
+import numpy as np
+import tensorflow as tf
+from PIL import Image
+from matplotlib import pyplot as plt
+
+import os
+
+class Resize_Images():
+
+  def __init__(self, **kwargs):
+     super().__init__(**kwargs)
+
+  def read_all_files(self, read_path, write_path, resize_with_pad = True):
+    for file in os.listdir(read_path):
+      image = tf.io.read_file(str(read_path + '/' + file))
+      if file.endswith(".png"):
+        image = tf.io.decode_png(image)
+      if file.endswith(".jpeg"):
+        image = tf.io.decode_jpeg(image)
+      
+      final_resized_image = self.resize_images_to_256(image, resize_with_pad)
+      #plt.imshow(final_resized_image)
+      final_resized_image.save(write_path + '/resized_' + file)
+      
+  
+  def resize_images_to_256(self, image, resize_with_pad):
+    if (resize_with_pad):
+      resized_image = tf.image.resize_with_pad(image, 256, 256,
+                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    else: 
+      resized_image = tf.image.resize(image, [256, 256],
+                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+    
+    resized_image = resized_image[:,:,0]
+
+    #convert to PIL image
+    pil_image = Image.fromarray(np.array(resized_image))
+
+    return pil_image
+
+def main():
+  read_minutiae_map_folder_path = '/content/drive/MyDrive/ITSEC/minutiaeMaps'
+  write_minutiae_map_folder_path = '/content/drive/MyDrive/ITSEC/Minutiae_Map_Images_256'
+
+  read_fingerprint_folder_path = '/content/drive/MyDrive/ITSEC/FingerPrints_1Channel'
+  write_fingerprint_folder_path = '/content/drive/MyDrive/ITSEC/Fingerprint_Images_256'
+
+  resize_images = Resize_Images()
+  resize_images.read_all_files(read_folder_path, write_folder_path, False)
+  resize_images.read_all_files(read_fingerprint_folder_path, write_fingerprint_folder_path, True)
+
+if __name__ == '__main__':
+  main()
