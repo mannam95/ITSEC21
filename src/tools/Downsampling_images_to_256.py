ca7aee6e5f9378e3775ca23a3c53329e8ef2ca11
Meghana Rao <76180138+meghanaraobn2020@users.noreply.github.com>
2021-12-14 19:17:56 +0100

    Updated the code to retain the size of the images

--- src/tools/Downsampling_images_to_256.py
+++ src/tools/Downsampling_images_to_256.py
@@ -1,8 +1,7 @@
-
 import numpy as np
 import tensorflow as tf
 from PIL import Image
-# from matplotlib import pyplot as plt
+from matplotlib import pyplot as plt
 
 import os
 
@@ -22,7 +21,6 @@ class Resize_Images():
       final_resized_image = self.resize_images_to_256(image, resize_with_pad)
       #plt.imshow(final_resized_image)
       final_resized_image.save(write_path + '/resized_' + file, dpi=(500, 500), quality=500)
-      break
       
   
   def resize_images_to_256(self, image, resize_with_pad):
@@ -31,7 +29,7 @@ class Resize_Images():
                                 method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
     else: 
       resized_image = tf.image.resize(image, [256, 256],
-                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR)
+                                method=tf.image.ResizeMethod.NEAREST_NEIGHBOR, antialias=True, preserve_aspect_ratio = True)
     
     resized_image = resized_image[:,:,0]
 
@@ -41,14 +39,14 @@ class Resize_Images():
     return pil_image
 
 def main():
-  read_minutiae_map_folder_path = '/content/drive/MyDrive/ITSEC/minutiaeMaps'
-  write_minutiae_map_folder_path = '/content/drive/MyDrive/ITSEC/Minutiae_Map_Images_256'
+  read_minutiae_map_folder_path = '../../data/initial_Pre_Process_data/FingerPrints/minutiaeMaps'
+  write_minutiae_map_folder_path = '../../data/initial_Pre_Process_data/FingerPrints/Minutiae_Map_Images_256'
 
   read_fingerprint_folder_path = '../../data/initial_Pre_Process_data/FingerPrints/FingerPrints_1Channel'
   write_fingerprint_folder_path = '../../data/initial_Pre_Process_data/FingerPrints/Fingerprint_Images_256'
 
   resize_images = Resize_Images()
-  # resize_images.read_all_files(read_folder_path, write_folder_path, False)
+  resize_images.read_all_files(read_folder_path, write_folder_path, False)
   resize_images.read_all_files(read_fingerprint_folder_path, write_fingerprint_folder_path, True)
 
 if __name__ == '__main__':
