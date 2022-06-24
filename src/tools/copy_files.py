ab9644c225031045ce00f5a507257aa4ef894f9d
Srinath Mannam <mvsrinath2009@gmail.com>
2022-06-24 12:07:31 +0200

    updated copy files to suppport different directory

--- src/tools/copy_files.py
+++ src/tools/copy_files.py
@@ -4,17 +4,18 @@ import PIL
 import shutil
 
 
-def copy_files(input_dir, output_dir):
+def copy_files(input_dir, source_dir, dest_dir):
     """This function copies the original files from the given train dataset. 
 
-    :param input_dir: source image directory.
-    :param output_dir: destination image directory.
+    :param input_dir: file names directory.
+    :param source_dir: source image directory.
+    :param dest_dir: destination image directory.
     :return: Returns None.
     """
     files = os.listdir(input_dir)
     for index, image_name in enumerate(files):
-        source_img_path = input_dir + "/" + image_name
-        dest_img_path = output_dir + "/" + image_name
+        source_img_path = source_dir + "/" + image_name
+        dest_img_path = dest_dir + "/" + image_name
         shutil.copy(source_img_path, dest_img_path)
 
 
@@ -22,20 +23,25 @@ def copy_files(input_dir, output_dir):
 def main():
 
     if ( len(sys.argv) < 3 ):
-        print('Usage: python '+sys.argv[0]+ ' <input_dir> <output_dir>')
-        print('\tinp_dir: this folder is the source image files.')
-        print('\tout_dir: this folder is the destination image files.')
+        print('Usage: python '+sys.argv[0]+ ' <inp_dir> <des_dir> <src_dir>')
+        print('\tinp_dir: this folder contains image files.')
+        print('\tdes_dir: this folder is the dest image files.')
+        print('\tsrc_dir: Use this option if source of the file is located in different directory with same file name located in input directory.')
         sys.exit(0)
 
     # parse command line parameters
     input_dir =  sys.argv[1]
+    src_dir = input_dir
 
-    output_dir = sys.argv[2]
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
+    des_dir = sys.argv[2]
+    if not os.path.exists(des_dir):
+        os.makedirs(des_dir)
+
+    if ( len(sys.argv) == 4 ):
+        src_dir = sys.argv[3]
 
     # copy files
-    copy_files(input_dir, output_dir)
+    copy_files(input_dir, src_dir, des_dir)
 
 
 if __name__ == '__main__':
